c3b463b3cee72b0ab4a80bf74d203b175d61a6b1
yonastekle433 <yonastekle433@gmail.com>
2022-04-09 08:29:45 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,6 @@
-/*
- * main - entry point
- * Auth:Yonas tekle
- * /
-#include <stdio.sh>
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzel");
-return(0);
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
 }
