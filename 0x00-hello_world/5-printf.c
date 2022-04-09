9b9ed163d1b19dc512dab86dd46c28e36ed3b22c
yonastekle433 <yonastekle433@gmail.com>
2022-04-09 09:10:41 -0700

    task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- *main - entry point 
+ *main - entry point
  *
- *Return : 0
+ *Return:0
 */
 int main(void)
 {
-printf("with proper grammer, but the outcome is a piece of art,\n");
+printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
