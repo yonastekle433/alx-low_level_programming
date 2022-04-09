2727b725220babb4a23434ee992e77b571069a6e
yonastekle433 <yonastekle433@gmail.com>
2022-04-09 08:56:09 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
+#include <stdio.h>
 /**
  *main - entry point
  *Return 0
 */
-#include <stdio.h>
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+        puts("\"Programming is like building a multilingual puzzle");
 
-	return (0);
+        return (0);
 }
