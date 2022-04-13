5bfc0011830ad6e7992350771c8f9d0e78adbee1
yonastekle433 <yonastekle433@gmail.com>
2022-04-13 07:23:32 -0700

    task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main -  entry point putchar function
+ * 
+ * Return: always 0
+*/
+
+int main(void)
+{
+           _putchar('A');
+           _putchar('L');
+           _putchar('X');
+	   _putchar('\n');
+
+           return(0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+My read me for functions and nested loops
