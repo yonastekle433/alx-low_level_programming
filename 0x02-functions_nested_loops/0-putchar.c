1e712ce1578acd926df557a662c189716e88a2d2
yonastekle433 <yonastekle433@gmail.com>
2022-04-13 12:55:50 -0700

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,19 +2,20 @@
 
 /**
  * main -  entry point putchar function
- * 
  * Return: always 0
 */
 
 int main(void)
 {
-           _putchar('_');
-           _putchar('p');
-           _putchar('u');
-	   _putchar('t');
-           _putchar('c');
-           _putchar('h');
-	   _putchar('a');
-	   _putchar('r');
-	   return(0);
+        _putchar('_');
+        _putchar('p');
+        _putchar('u');
+	_putchar('t');
+        _putchar('c');
+        _putchar('h');
+        _putchar('a');
+	_putchar('r');
+	_putchar('\n')
+        return(0);
 }
+
