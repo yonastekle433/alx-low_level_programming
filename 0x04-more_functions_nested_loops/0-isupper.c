231c71436f448dcd8990f8ae1950a871615366b2
yonastekle433 <yonastekle433@gmail.com>
2022-04-17 10:13:48 -0700

    task0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - entry point
+ * 
+ * Return: always 0
+*/
+
+int _isupper(int c);
+{
+     if (c >= 'A' && c<= 'Z')
+             return (1);
+ 
+     else
+          return (0);
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+My read me for more functions and nested loops
