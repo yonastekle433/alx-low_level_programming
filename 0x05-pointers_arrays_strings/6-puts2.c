8a5447bbf92531b8db1331a2550b0e7d6be1a804
yonastekle433 <yonastekle433@gmail.com>
2022-04-18 12:16:08 -0700

    task 5

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,15 +1,18 @@
 #include "main.h"
-
+#include <stdio.h>
 /**
- * main - check the code for Holberton School students.
- *
+ * puts2 - check the code for Holberton School students.
+ * @str: pointer int type
  * Return: Always 0.
  */
-int main(void)
+void puts2(char *str)
 {
-    char *str;
-
-    str = "0123456789";
-    puts2(str);
-    return (0);
+int i;
+for (i = 0; *str != '\0'; i++)
+{
+if (i % 2 == 0)
+_putchar(*str);
+str++;
+}
+_putchar('\n');
 }
