4414c7540502d36157e030702e07bb445ab63ddd
yonastekle433 <yonastekle433@gmail.com>
2022-04-20 21:18:08 -0700

    task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -2,23 +2,23 @@
 #include <stdio.h>
 
 /**
- * _strcat - check the code for students
+ * _strcat - check the code for Holberton School students.
  * @dest: is a pointer type char
- * @src : is a pointer type char
- * Return: always 0
+ * @src: is a pointer type char
+ * Return: Always 0.
  */
 char *_strcat(char *dest, char *src)
 {
 int i, j;
-for(i=0; *dest != 0; i++)
+for (i = 0; *dest != 0; i++)
 dest++;
 
-for(j=0; src[j] != 0; j++)
+for (j = 0; src[j] != 0; j++)
 {
 *dest = src[j];
 dest++;
 }
 *dest = '\0';
 dest -= (i + j);
-rerturn (dest/0;
+return (dest);
 }
