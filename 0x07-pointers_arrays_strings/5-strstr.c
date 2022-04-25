9901425cee47215a294bcdcd996fcb228b4b5dac
yonastekle433 <yonastekle433@gmail.com>
2022-04-25 08:01:47 -0700

    tasks

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,48 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strstr - locates a character in a string
+ * @haystack: is a pointer type char
+ * @needle: is a pointer type char
+ * Return: The number of bytes repeated
+ */
+char *_strstr(char *haystack, char *needle)
+{
+int i, num = 0, cont;
+
+for (cont = 0; needle[cont] != '\0'; cont++)
+;
+
+for (; *haystack != '\0'; haystack++)
+{
+if (*haystack == needle[0])
+{
+num++;
+haystack++;
+for (i = 1; i < cont; i++, haystack++)
+{
+if (*haystack == needle[i])
+num++;
+
+else
+{
+haystack--;
+num = 0;
+break;
+}
+
+}
+}
+
+if (num == cont)
+break;
+
+}
+
+
+if (num == cont)
+return (haystack - cont);
+else
+return (0);
+}
---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_chessboard - locates a character in a string
+ * @a: is a pointer type char
+ * Return: The number of bytes repeated
+ */
+void print_chessboard(char (*a)[8])
+{
+int i, j;
+for (i = 0; i < 8; i++)
+{
+for (j = 0; j < 8; j++)
+{
+_putchar(a[i][j]);
+}
+_putchar('\n');
+}
+}
