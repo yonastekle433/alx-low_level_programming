de53996d4da4962a0256cb7e3359d5a6a3df87f4
yonastekle433 <yonastekle433@gmail.com>
2022-04-27 22:24:23 -0700

    task 0

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,19 @@
+#ifndef MAIN_FUNTIONS
+#define MAIN_FUNTIONS
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c);  /* An example function declaration */
+void _puts_recursion(char *s);
+void _print_rev_recursion(char *s);
+int _strlen_recursion(char *s);
+int factorial(int n);
+int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
+int is_prime_number(int n);
+int is_palindrome(char *s);
+#endif
