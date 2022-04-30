aacc7cdce8d7f85f4f6f856abe33b12122b1f22b
yonastekle433 <yonastekle433@gmail.com>
2022-04-29 21:52:09 -0700

    task 0

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,59 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int power(int b);
-
 /**
-  * main - print the multiplication of two arguments
-  * @argc: has the length of the arguments
-  * @argv: has the arguments
-  * Return: 0 to indicate a good working of the program
-  */
-
-int main(int argc, char **argv)
+ * main - multiplies two numbers
+ * @argc: number of arguments passed to the function
+ * @argv: argument vector of pointers to strings
+ *
+ * Return: 0 if no errors, else 1
+ */
+int main(int argc, char *argv[])
 {
+	int a, b, c;
 
-	int i, j, cont, num[2] = {0, 0};
-
-	if (argc < 3)
-		printf("Error\n");
-
-	else
+	if (argc != 3)
 	{
-		for (i = 1; argv[i] != '\0'; i++)
-		{
-			for (cont = 0; (argv[i])[cont] != '\0'; cont++)
-				;
-
-			for (j = 0, cont--; (argv[i])[j] != '\0'; j++)
-			{
-				if ((argv[i])[cont - j] == '-')
-				{
-					num[i - 1] *= -1;
-					break;
-				}
-				else
-					num[i - 1] = power(j) * ((argv[i])[cont - j] - '0') + num[i - 1];
-			}
-		}
-		printf("%d\n", num[0] * num[1]);
+		puts("Error");
+		return (1);
 	}
+	a = atoi(argv[1]);
+	b = atoi(argv[2]);
+	c = a * b;
+	printf("%d\n", c);
 	return (0);
-}
-
-/**
-  * power - get the power 10 times b
-  * @b: is the times of 10
-  * Return: the power 10 times b
-  */
-
-int power(int b)
-{
-	int i, num = 1;
-
-	for (i = 0; i < b; i++)
-	{
-		num *= 10;
-	}
-	return (num);
 }
